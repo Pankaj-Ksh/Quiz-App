1b82f368e8fb2128f5f9501ed9daf13c63df9c87
Pankaj-Ksh <pankajksh00@gmail.com>
2024-02-01 22:52:34 +0530

    React Quiz App

---
+++ src/Data/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question: "What is React?",
+        options: [
+            "A JavaScript library for building user interfaces",
+            "A programming language",
+            "An operating system",
+            "A database management system"
+        ],
+        answer: 1
+    },
+    {
+        question: "Which company developed React?",
+        options: [
+            "Google",
+            "Facebook",
+            "Microsoft",
+            "Apple"
+        ],
+        answer: 2
+    },
+    {
+        question: "What is JSX?",
+        options: [
+            "A JavaScript framework",
+            "A markup syntax that lets you write HTML elements in JavaScript",
+            "A CSS preprocessor",
+            "A package manager"
+        ],
+        answer: 2
+    },
+    {
+        question: "What does the useEffect hook do in React?",
+        options: [
+            "It fetches data from an API",
+            "It updates the state of a component",
+            "It performs side effects in function components",
+            "It adds event listeners to DOM elements"
+        ],
+        answer: 3
+    },
+    {
+        question: "What is the purpose of React Router?",
+        options: [
+            "To manage state in React components",
+            "To create reusable UI components",
+            "To handle navigation in single-page applications",
+            "To style React applications"
+        ],
+        answer: 3
+    }
+];
